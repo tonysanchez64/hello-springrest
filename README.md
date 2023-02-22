cd34d9557d780e34aacc83a510abfc99a59d10b1
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2023-02-22 04:52:40 -0600

    Initial import

---
+++ README.md
@@ -0,0 +1,8 @@
+# hello-springrest
+
+Proyecto de ejemplo de API REST con Spring Boot.
+
+* Construya un `Dockerfile` que ilustra el concepto de multi-stage builds.
+* Construya un pipeline CI/CD para Jenkins.
+* Integre los resultados de las pruebas para visualizar los resultados en Jenkins.
+* Despliegue en AWS ElasticBeanstalk.
---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/")
+	public Greeting root() {
+		return new Greeting(counter.incrementAndGet(),"Hola ke ase");
+	}
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
